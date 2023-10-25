fe52907d8c0132ee4d7f846dc8fcfc6bd019e2cb
Jack <78124447+JackDerksen@users.noreply.github.com>
2023-10-25 12:49:28 -0700

    Add files via upload

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- Options are automatically loaded before lazy.nvim startup
+
 vim.opt.guicursor = ""
 vim.opt.cursorline = false
 
