936f31458131fe64a4ef8158e482dbd932103638
Jack <78124447+JackDerksen@users.noreply.github.com>
2023-11-18 15:27:23 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ require("config.lazy")
 
 vim.cmd.colorscheme("catppuccin")
 
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6e738d", bold = true })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6e738d", bold = false })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#b4befe", bold = true })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6e738d", bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6e738d", bold = false })
