830e3c3e22c8fc7975dfc279beb52ab872e41c91
Jack <78124447+JackDerksen@users.noreply.github.com>
2024-02-03 13:11:59 -0700

    Add files via upload

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -12,7 +12,7 @@ return {
     - viis -
     ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    logo = string.rep("\n", 4) .. logo .. "\n\n"
 
     local opts = {
       theme = "doom",
@@ -28,10 +28,8 @@ return {
           { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
           { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
           { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
-          { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
           { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
           { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "e" },
           { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
           { action = "qa", desc = " Quit", icon = " ", key = "q" },
         },
