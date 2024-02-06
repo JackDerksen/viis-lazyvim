3620a1215cf25664bd65242b7c7e73b7f8cd053b
Jack <78124447+JackDerksen@users.noreply.github.com>
2024-02-06 14:52:48 -0700

    Updated init.lua file for random lineNr highlight

--- init.lua
+++ init.lua
@@ -2,6 +2,33 @@ require("config.lazy")
 
 vim.cmd.colorscheme("catppuccin")
 
+-- This is the old line number highlight behaviour
+--vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6e738d", bold = false })
+--vim.api.nvim_set_hl(0, "LineNr", { fg = "#b4befe", bold = true })
+--vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6e738d", bold = false })
+
+-- Function to set a random highlight color for LineNr
+function SetRandomLineNrColor()
+  math.randomseed(os.time())
+
+  local colors = {
+    "#b4befe", -- Lavender
+    "#eba0ac", -- Maroon
+    "#d2fac5", -- Green
+    "#f2cdcd", -- Flamingo
+    "#cba6f7", -- Mauve
+    "#fcc6a7", -- Peach
+    "#89b4fa", -- Blue
+    "#89dceb", -- Sky
+  }
+
+  local index = math.random(#colors)
+  vim.api.nvim_set_hl(0, "LineNr", { fg = colors[index], bold = true })
+end
+
+-- Ensure the random color is selected each time Neovim starts
+vim.cmd("autocmd VimEnter * lua SetRandomLineNrColor()")
+
+-- Setting highlights for lines above and below
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6e738d", bold = false })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#b4befe", bold = true })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6e738d", bold = false })
